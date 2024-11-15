8b9ee73a61c6d95db713742abc5989f8a9fdf864
GemHou <784210432@qq.com>
2024-11-15 14:29:49 +0800

    hj: freq

--- main_test9_wandb.py
+++ main_test9_wandb.py
@@ -15,6 +15,10 @@ DEVICE = torch.device("cpu")
 def collect_experience_once(ac, env, local_steps_per_epoch, max_ep_len, replay_buffer, list_ep_ret):
     obs_ma, info = env.reset()
     ep_ret, ep_len = 0, 0
+    list_epoch_ep_ret = []
+    list_epoch_ep_len = []
+    list_epoch_rps = []
+    list_epoch_reset_time = []
     for t in range(local_steps_per_epoch):
         obs_tensor = torch.tensor(obs_ma[0], dtype=torch.float32)
         action, v, logp = ac.step(obs_tensor)
@@ -54,14 +58,22 @@ def collect_experience_once(ac, env, local_steps_per_epoch, max_ep_len, replay_b
             replay_buffer.finish_path(v)
             # print("ep_ret: ", ep_ret)
             list_ep_ret.append(ep_ret)
-            wandb.log({"5 performance/episode return": ep_ret})
-            wandb.log({"5 performance/episode length": ep_len})
-            wandb.log({"5 performance/return per step": ep_ret/ep_len})
+            # wandb.log({"5 performance/episode return": ep_ret})
+            # wandb.log({"5 performance/episode length": ep_len})
+            # wandb.log({"5 performance/return per step": ep_ret/ep_len})
+            list_epoch_ep_ret.append(ep_ret)
+            list_epoch_ep_len.append(ep_len)
+            list_epoch_rps.append(ep_ret / ep_len)
             reset_time_start = time.time()
             obs_ma, info = env.reset()
             reset_time = time.time() - reset_time_start
-            wandb.log({"8 throughout/AverageResetTime": reset_time})
+            # wandb.log({"8 throughout/AverageResetTime": reset_time})
+            list_epoch_reset_time.append(reset_time)
             ep_ret, ep_len = 0, 0
+    wandb.log({"5 performance/episode return": np.mean(list_epoch_ep_ret)})
+    wandb.log({"5 performance/episode length": np.mean(list_epoch_ep_len)})
+    wandb.log({"5 performance/return per step": np.mean(list_epoch_rps)})
+    wandb.log({"8 throughout/AverageResetTime": np.mean(list_epoch_reset_time)})
 
 
 def compute_loss_pi_with_entropy(data, ac, clip_ratio,
@@ -103,26 +115,30 @@ def update(data, ac, clip_ratio, train_pi_iters, train_v_iters, pi_optimizer, vf
     pi_l_old = pi_l_old.item()
     v_l_old = compute_loss_v(data, ac).item()
     pi_info = None
+    list_epoch_loss_pi = []
+    list_epoch_loss_v = []
     for i in range(train_pi_iters):
         pi_optimizer.zero_grad()
         loss_pi, pi_info = compute_loss_pi_with_entropy(data, ac, clip_ratio)  #
-        # print("torch.cuda.is_available(): ", torch.cuda.is_available())
-
+        list_epoch_loss_pi.append(loss_pi.detach().numpy())
         # kl = mpi_avg(pi_info['kl'])
         kl = pi_info['kl']
         if kl > 1.5 * target_kl:
             # print('Early stopping at step %d due to %f reaching max kl.' % (i, kl))
             break
         loss_pi.backward()  # time!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        wandb.log({"7 spup/LossPi": loss_pi})
+        # wandb.log({"7 spup/LossPi": loss_pi})
         # mpi_avg_grads(ac.pi)  # average grads across MPI processes
         pi_optimizer.step()
     for i in range(train_v_iters):
         vf_optimizer.zero_grad()
         loss_v = compute_loss_v(data, ac)
-        wandb.log({"7 spup/LossV": loss_v})
+        # wandb.log({"7 spup/LossV": loss_v})
+        list_epoch_loss_v.append(loss_v.detach().numpy())
         loss_v.backward()
         vf_optimizer.step()
+    wandb.log({"7 spup/LossPi": np.mean(list_epoch_loss_pi)})
+    wandb.log({"7 spup/LossV": np.mean(list_epoch_loss_v)})
 
     kl, ent, cf = pi_info['kl'], pi_info_old['ent'], pi_info['cf']
     wandb.log({"7 spup/kl": kl})
@@ -131,7 +147,7 @@ def update(data, ac, clip_ratio, train_pi_iters, train_v_iters, pi_optimizer, vf
 
 
 def main():
-    local_steps_per_epoch = 1000
+    local_steps_per_epoch = 500
     max_ep_len = 50
     clip_ratio = 0.1
     train_pi_iters = 80
@@ -145,7 +161,7 @@ def main():
     wandb.init(
         # mode="offline",
         project="project-drone-20241115",
-        resume="more-20241115-1356"  # HjScenarioEnv
+        resume="bs500-20241115"  # HjScenarioEnv
     )
 
     env = HoverAviary(gui=False)
@@ -174,16 +190,11 @@ def main():
         wandb.log({"8 throughout/LifeLongEnvRate": (epoch + 1) * local_steps_per_epoch / life_long_time})
         wandb.log({"8 throughout/LifeLongUpdateRate": (epoch + 1) * train_pi_iters / life_long_time})
 
-        # plt.cla()
-        # plt.plot(list_ep_ret)
-        # plt.pause(0.0000000001)
-
         data = replay_buffer.get(device=DEVICE)
 
         update(data, ac, clip_ratio, train_pi_iters, train_v_iters, pi_optimizer, vf_optimizer, target_kl)
 
     print("Finished...")
-    # plt.show()
 
 
 if __name__ == "__main__":
