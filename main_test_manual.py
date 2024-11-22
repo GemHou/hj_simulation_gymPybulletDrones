f936023dc6454baec1e7d3868b294c89acb51ce2
GemHou <784210432@qq.com>
2024-11-22 13:48:37 +0800

    hj: goal vel_z

---
+++ main_test_manual.py
@@ -0,0 +1,57 @@
+import time
+
+import torch
+import numpy as np
+
+from utils_rl import MLPActorCritic
+from utils_drone import HjAviary
+
+DEVICE = torch.device("cpu")
+
+
+def main():
+    env = HjAviary(gui=True)
+
+    ac = MLPActorCritic(env.observation_space, env.action_space)
+
+    state_dict = torch.load("./data/interim/para_temp.pt",
+                            map_location=torch.device(DEVICE))
+    ac.load_state_dict(state_dict)
+
+    for i in range(10):
+        obs_ma, info = env.reset()
+        for j in range(200):
+            if False:
+                obs_tensor = torch.tensor(obs_ma[0], dtype=torch.float32)
+                action, _, _ = ac.step(obs_tensor)
+            else:
+                obs_12 = obs_ma[:, :12]
+                pos = obs_12[:, 0:3]
+                rpy = obs_12[:, 3:6]
+                vel = obs_12[:, 6:9]
+                ang = obs_12[:, 9:12]
+                pos_z = pos[0, 2]
+                vel_z = vel[0, 2]
+                if True:
+                    print("vel_z: ", vel_z)
+                    if vel_z < 1:
+                        action_z = 1
+                    else:
+                        action_z = -1
+                else:
+                    action_z = 0
+                action = [action_z, action_z, action_z, action_z]
+            action_ma = np.array([action])
+            next_obs_ma, reward, done, truncated, info = env.step(
+                action_ma)  # obs [1, 72] 12 + ACTION_BUFFER_SIZE * 4 = 72
+            obs_ma = next_obs_ma
+
+            env.render()
+            time.sleep(1/30)
+
+            if done:
+                break
+
+
+if __name__ == "__main__":
+    main()
