5c7656923a1452aa899b0387280675f247b7f034
GemHou <784210432@qq.com>
2024-12-02 17:56:15 +0800

    hj: CosineAnnealingLR

--- main_train_actionMotor.py
+++ main_train_actionMotor.py
@@ -3,14 +3,14 @@ import tqdm
 import wandb
 import torch
 from torch.optim import Adam
-# import matplotlib.pyplot as plt
+from torch.optim.lr_scheduler import CosineAnnealingLR
 
 from utils_drone import HjAviary
 from utils_rl import PPOBuffer, MLPActorCritic, collect_experience_once, update
 
 DEVICE = torch.device("cpu")
-RESUME_NAME = "actionMotor-x10-reward1-len1000-20241202"
-
+RESUME_NAME = "actionMotor-x10-reward1-lrcos-len1000-20241202"
+EPOCH = 1000
 
 def main():
     local_steps_per_epoch = 2000
@@ -18,8 +18,8 @@ def main():
     clip_ratio = 0.1
     train_pi_iters = 80
     train_v_iters = 80
-    pi_lr = 3e-4
-    vf_lr = 1e-3
+    pi_lr = 3e-4  # 初始学习率
+    vf_lr = 1e-3  # 固定学习率
     target_kl = 0.01
 
     life_long_time_start = time.time()
@@ -50,9 +50,13 @@ def main():
     pi_optimizer = Adam(ac.pi.parameters(), lr=pi_lr)
     vf_optimizer = Adam(ac.v.parameters(), lr=vf_lr)
 
+    # 添加 Cosine Annealing 调度器
+    scheduler_pi = CosineAnnealingLR(optimizer=pi_optimizer, T_max=EPOCH, eta_min=1e-5)  # T_max 设为总 epoch 数
+    scheduler_vf = CosineAnnealingLR(optimizer=vf_optimizer, T_max=EPOCH, eta_min=1e-5)  # T_max 设为总 epoch 数
+
     list_ep_ret = []
 
-    for epoch in tqdm.tqdm(range(1000)):
+    for epoch in tqdm.tqdm(range(EPOCH)):
         wandb.log({"7_1 spup increase/Epoch": (epoch + 1)})
         collect_experience_once(ac, env, local_steps_per_epoch, max_ep_len, replay_buffer, list_ep_ret)
         wandb.log({"7_1 spup increase/TotalEnvInteracts": (epoch + 1) * local_steps_per_epoch})
@@ -65,10 +69,13 @@ def main():
 
         update(data, ac, clip_ratio, train_pi_iters, train_v_iters, pi_optimizer, vf_optimizer, target_kl)
 
+        # 调整学习率
+        scheduler_pi.step()
+        scheduler_vf.step()
+
         torch.save(ac.state_dict(), "./data/interim/para_actionMotor_temp.pt")
 
     print("Finished...")
-
 
 if __name__ == "__main__":
     main()
