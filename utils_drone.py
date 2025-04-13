daeb3381ddb0c5c31da9306befc743a9de0894cd
houjinghp <784210432@qq.com>
2025-04-13 15:32:16 +0800

    hj: try: scenario

--- utils_drone.py
+++ utils_drone.py
@@ -45,7 +45,7 @@ class HjAviary(HoverAviary):
 
         reward_ang_v = 1 / (abs(ang_v[2]) / 1 + 1)
 
-        reward = reward_done + reward_target * 0.8 + reward_ang_v * 0.2  # + reward_z + reward_xy
+        reward = reward_done + reward_target * 0.7 + reward_ang_v * 0.3  # + reward_z + reward_xy
 
         reward = np.clip(reward, -1, 2)
 
