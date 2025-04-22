2ce28d482eb6f3df4df665aa5996b5b695eda279
houjinghp <784210432@qq.com>
2025-04-22 08:31:41 +0800

    hj: occ_array.npy

--- main_ray.py
+++ main_ray.py
@@ -9,41 +9,13 @@ import matplotlib.pyplot as plt
 from utils_rl import MLPActorCritic
 from utils_drone import HjAviary
 
-DEVICE = torch.device("cpu")
-CONTROL_MODE = "RL"  # RL
 PERCENT = 1.0
-MAX_EP_LEN = 1000
-LOAD_PATH = "./data/interim/para_randomTMove_obs81_scenario_39.pt"  # _041212
 RENDER = False
 NUM_X = 128 * 6  # 6
 NUM_Y = 128 * 6  # 6
 NUM_Z = 128
+SAVE_path = "./data/occ_array.npy"
 
-
-def main():
-    wandb.init(
-        project="project-drone-test-20241122",
-    )
-    env = HjAviary(gui=RENDER)  # , ctrl_freq=10, pyb_freq=100
-
-    obs_ma, info = env.reset(PERCENT)
-
-    for i in tqdm(range(NUM_X)):
-        x = (i - NUM_X/2) * 0.25
-        for j in range(NUM_Y):
-            y = (j - NUM_Y/2) * 0.25
-            for k in range(NUM_Z):
-                z = k * 0.25
-                ray_from = [x, y, z]
-                ray_to = [x, y, z+0.25]
-                ray_results = p.rayTest(ray_from, ray_to)
-                if ray_results[0][0]!=-1:
-                    pass
-                    # print("ray_results: ", ray_results)
-                    if RENDER:
-                        draw_ball(ray_results[0][3])
-
-    time.sleep(99999)
 
 
 def draw_ball(pos):
@@ -64,6 +36,35 @@ def draw_ball(pos):
         baseVisualShapeIndex=visual_shape_id,
         basePosition=position
     )
+
+
+def main():
+    wandb.init(
+        project="project-drone-test-20241122",
+    )
+    env = HjAviary(gui=RENDER)  # , ctrl_freq=10, pyb_freq=100
+
+    obs_ma, info = env.reset(PERCENT)
+
+    occ_array = np.zeros((NUM_X, NUM_Y, NUM_Z))
+
+    for i in tqdm(range(NUM_X)):
+        x = (i - NUM_X/2) * 0.25
+        for j in range(NUM_Y):
+            y = (j - NUM_Y/2) * 0.25
+            for k in range(NUM_Z):
+                z = k * 0.25
+                ray_from = [x, y, z]
+                ray_to = [x, y, z+0.25]
+                ray_results = p.rayTest(ray_from, ray_to)
+                if ray_results[0][0]!=-1:
+                    occ_array[i][j][k] = 1
+                    # print("ray_results: ", ray_results)
+                    if RENDER:
+                        draw_ball(ray_results[0][3])
+    print("np.sum(occ_array): ", np.sum(occ_array))
+
+    np.save(SAVE_path, occ_array)
 
 
 if __name__ == "__main__":
