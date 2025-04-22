04ed9ae34d0b8d229df78a62a6699bb5dc75cdb4
houjinghp <784210432@qq.com>
2025-04-22 12:59:16 +0800

    hj: o3d

--- main_a_star.py
+++ main_a_star.py
@@ -1,36 +1,29 @@
 import numpy as np
 import open3d as o3d
-import pybullet as p
 from tqdm import tqdm
 
 
 def draw_ball(pos, color=None):
     # 定义球体的参数
     if color is None:
-        color = [1, 0, 0, 1]
-    position = pos  # 球体的中心位置
-    radius = 0.1  # 球体的半径
-    # 创建球体的视觉形状
-    visual_shape_id = p.createVisualShape(
-        shapeType=p.GEOM_SPHERE,
-        radius=radius,
-        rgbaColor=color
-    )
-    # 创建球体的多体对象
-    ball_id = p.createMultiBody(
-        baseMass=0,  # 质量为0，表示这是一个静态物体
-        baseCollisionShapeIndex=-1,  # 不需要碰撞形状
-        baseVisualShapeIndex=visual_shape_id,
-        basePosition=position
-    )
+        color = [1, 0, 0]  # 默认颜色为红色
+    radius = 10  # 球体的半径
+
+    # 创建球体
+    sphere = o3d.geometry.TriangleMesh.create_sphere(radius=radius)
+    sphere.translate(pos)  # 将球体移动到指定位置
+    sphere.paint_uniform_color(color)  # 设置球体颜色
+
+    return sphere
 
 
 def main():
     start_point = [0, 0, 0]
     target_point = [10, 10, 10]
 
-    draw_ball(start_point, color=[1, 0, 0, 1])
-    draw_ball(target_point, color=[0, 1, 0, 1])
+    # 绘制球体
+    start_sphere = draw_ball(start_point, color=[1, 0, 0])  # 红色球体
+    target_sphere = draw_ball(target_point, color=[0, 1, 0])  # 绿色球体
 
     occ_file_path = "./data/occ_array.npy"
     occ_array = np.load(occ_file_path)
@@ -47,8 +40,13 @@ def main():
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(np.array(points))
 
-    # 可视化点云
-    o3d.visualization.draw_geometries([pcd], window_name="3D Occupancy Visualization", width=800, height=600)
+    # 可视化点云和球体
+    o3d.visualization.draw_geometries(
+        [pcd, start_sphere, target_sphere],
+        window_name="3D Occupancy Visualization",
+        width=800,
+        height=600
+    )
 
     print("Finished...")
 
