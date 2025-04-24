bc1da294f369881a6f0089ae7f6474c8e8277708
houjinghp <784210432@qq.com>
2025-04-24 11:17:08 +0800

    hj: tensorboard

--- main_imiation.py
+++ main_imiation.py
@@ -3,8 +3,9 @@ import numpy as np
 from tqdm import tqdm
 import torch.nn as nn
 import torch.optim as optim
+from datetime import datetime
 from torch.utils.data import Dataset, DataLoader
-
+from torch.utils.tensorboard import SummaryWriter  # 导入 TensorBoard 的 SummaryWriter
 
 # 自定义数据集类
 class TrajectoryDataset(Dataset):
@@ -57,19 +58,36 @@ def main():
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
+    # 初始化 TensorBoard 的 SummaryWriter
+    current_time = datetime.now().strftime('%Y%m%d_%H%M%S')
+    writer = SummaryWriter(log_dir=('./runs/trajectory_experiment'+current_time))  # 指定日志保存路径 tensorboard --logdir=./runs
+
     # 训练模型
     print("Training...")
     num_epochs = 10
-    for epoch in tqdm(range(num_epochs)):
+    for epoch in range(num_epochs):
         running_loss = 0.0
+        batch_idx = 0
         for drone_pos, target_pos, future_traj in tqdm(dataloader):
+            batch_idx += 1
             optimizer.zero_grad()
             outputs = model(drone_pos, target_pos)
             loss = criterion(outputs, future_traj)
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
-        print(f'Epoch {epoch + 1}, Loss: {running_loss / len(dataloader)}')
+
+            # 每个批次记录损失到 TensorBoard
+            writer.add_scalar('Training Loss per Batch', loss.item(), epoch * len(dataloader) + batch_idx)
+
+        # 每个 epoch 记录平均损失到 TensorBoard
+        avg_loss = running_loss / len(dataloader)
+        writer.add_scalar('Training Loss per Epoch', avg_loss, epoch)
+
+        print(f'Epoch {epoch + 1}, Loss: {avg_loss}')
+
+    # 关闭 TensorBoard 的 SummaryWriter
+    writer.close()
 
 
 if __name__ == "__main__":
